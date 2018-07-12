92f0e360b75b90bebaee215f3093d4153fb2ed89
James Ferguson <ncjamesferguson@gmail.com>
2018-07-12 00:42:43 +0000

    automatically backed up by learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -3,6 +3,11 @@ import React from 'react';
 
 export default class DelayedButton extends React.Component {
 
+  handleClick = (event) => {
+    event.persist();
+    this.props.onDelayedClick
+  }
+
   render() {
     return (
 
